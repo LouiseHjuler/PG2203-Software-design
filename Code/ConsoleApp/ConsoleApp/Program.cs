0d9fa72d66dca5c1618ac233f84bb5c6dea35e04
LouiseHjuler <lola003@student.kristiania.no>
2023-09-29 19:16:01 +0200

    init commit

---
+++ Code/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.CompilerServices;
+
+internal class Program
+{
+	private static void Main(string[] args) {
+		person person = new();
+		ABetterPerson aBetterPerson = new();
+
+		//person.setFirstname("Louise");
+		aBetterPerson.Firstname = "Louise";
+
+		//Console.WriteLine($"hello, {person.getFirstname()}");
+		Console.WriteLine($"hello, {aBetterPerson.Firstname}");
+	}
+
+    public class ABetterPerson {
+
+		// _ naming conventions Private classes should have an underscore
+		//in front of the name. can be used instead of "this."
+
+		public string Firstname { get; set; }
+		private string _lastname;
+		public string Lastname {
+			get {
+				return _lastname.Trim();
+			}
+			set {
+				if (value.Length>=5) {
+					_lastname = value;
+				} else {
+					_lastname = string.Empty; 
+				}
+			}
+		}
+	}
+	public class person {
+		private string firstname;
+		private string lastname;
+
+		public string getFirstname() {
+			return firstname;
+		}
+
+		public void Setname(string name) {
+			this.firstname = name;
+		}
+
+		
+		public string getName() {
+			return lastname.TrimEnd;
+		}
+
+		public void setname(string name) {
+			if (name.Length >= 5) {
+				this.lastname = name;
+			}
+
+		}
+	}
+}
---
+++ Code/Exerxise2  Task1a/Exerxise2  Task1a/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace Exerxise2__Task1a {
+	internal class Program {
+		static void Main(string[] args) {
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
